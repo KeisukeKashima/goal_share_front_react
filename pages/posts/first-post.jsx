9e1687fa44b7b3ad6d90ec462bc6ba6c409cf45a
KeisukeKashima <k2041kei@gmail.com>
2021-12-25 15:09:45 +0900

    add ant-design

--- pages/posts/first-post.jsx
+++ pages/posts/first-post.jsx
@@ -1,7 +1,10 @@
+import { Button } from 'antd';
+
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Button type="primary">Button</Button>
       <div>
         <p>hgoe1</p>
         <p>hgoe2</p>
