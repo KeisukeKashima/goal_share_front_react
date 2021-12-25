faba74d50224e67f3027a37664ffd32de5d31ae3
KeisukeKashima <k2041kei@gmail.com>
2021-12-25 14:39:23 +0900

    mod jsx_test

---
+++ pages/posts/first-post.jsx
@@ -0,0 +1,15 @@
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <div>
+        <p>hgoe1</p>
+        <p>hgoe2</p>
+      </div>
+      <div>
+        <p>fuga1</p>
+        <p>fuga2</p>
+      </div>
+    </>
+    )
+}
