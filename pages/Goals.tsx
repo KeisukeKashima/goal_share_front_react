dfa55db9f4ae15668f193072827be0fbd6848c9c
KeisukeKashima <k2041kei@gmail.com>
2021-12-27 09:30:34 +0900

    add タイトルコンポーネント追加とprops利用

--- pages/Goals.tsx
+++ pages/Goals.tsx
@@ -2,16 +2,10 @@ import React, {FC, useEffect, useState} from 'react';
 import {Button} from 'antd';
 import styles from 'styles/pages/Goals.module.scss'
 import Goal from "types/goal"
+import PageTitle from "../components/atoms/PageTitle";
 import { axiosClient } from "util/axiosClient"
 
-// FirstPost = () => { にして'react'のimportしなくてもいけた
 const Goals: FC = () => {
-  // Vueで言うdataの代わりにuseStateを利用する
-  // const [変数名, 変数に値をセットする際のメソッド名] = useState><型(tsの場合でjsは省略可)>(初期値)
-  // html側で表示する場合は「{}」でくくる <p>{ 変数名 }</p>
-  // 「{}」でくくった後にさらにhtmlを表示する場合は「return」が必要なので注意(はまった)
-  // const [count, setCount] = useState<number>(0)
-
   const [goals, setGoals] = useState<Goal[]>([])
 
   useEffect(() => {
@@ -24,6 +18,7 @@ const Goals: FC = () => {
   return (
     <>
       <h1>Goals</h1>
+      <PageTitle title={'目標一覧'}/>
       <Button type="primary">Button</Button>
       <div>
         <div>
