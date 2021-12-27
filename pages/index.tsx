ad2e8f89802c01a838b9bdc4404ce76fc7c2c3d0
KeisukeKashima <k2041kei@gmail.com>
2021-12-27 11:17:35 +0900

    add 初回アクセスページにもLayout追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,16 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import {Button} from 'antd';
 import React from "react";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <>
+    <Layout>
       <div className={styles.top}>
         <Link href='/Goals'>
           <Button type="primary">さぁ！まずはここからみんなの目標を覗いてみよう！</Button>
         </Link>
       </div>
-    </>
+    </Layout>
   )
 }
