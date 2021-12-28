27fba2203c914a86d2b611368ca1f0b78df38bdc
KeisukeKashima <k2041kei@gmail.com>
2021-12-28 11:48:36 +0900

    add ユーザ詳細追加

---
+++ pages/users/[user_id].tsx
@@ -0,0 +1,81 @@
+import React, {FC, useEffect, useState} from "react";
+import Layout from "../../components/Layout";
+import PageTitle from "../../components/atoms/PageTitle";
+import {Card} from 'antd';
+import {axiosClient} from "../../util/axiosClient";
+import {useRouter} from 'next/router';
+import commonStyles from "../../styles/common.module.css";
+import Link from "next/link";
+import User from "../../types/user";
+
+import {Avatar} from 'antd';
+import {UserOutlined} from '@ant-design/icons';
+
+const UserId: FC = () => {
+  const router = useRouter()
+  // TODO ログインユーザID取得処理
+  const [loginUserId, setLoginUserId] = useState<number>()
+  const userId = router.query.user_id
+  const [user, setUser] = useState<User>()
+
+  useEffect(() => {
+    axiosClient.get(`/api/users/${userId}`).then(res => {
+      setUser(res.data)
+    })
+  }, [])
+
+  return (
+    <Layout>
+      <div>
+        <PageTitle title={'ユーザ詳細'}/>
+        <Card
+          title={user.display_name}
+          bordered={true}
+        >
+          <p>■プロフィール写真</p>
+          <Avatar size="large" icon={<UserOutlined/>}/>
+
+          <div className={commonStyles.mgt20}/>
+
+          <p>■年齢</p>{user.age}
+
+          <div className={commonStyles.mgt20}/>
+
+          <p>■性別</p>{user.sex ? '男性' : '女性'}
+
+          <div className={commonStyles.mgt20}/>
+
+          {/*このユーザが登録済みの目標一覧*/}
+          <>
+            {
+              user.Goal.length > 0 &&
+              user.Goal.map((goal, index) => {
+                return (
+                  <Card
+                    title={goal.title}
+                    bordered={true}
+                    key={index}
+                  >
+                    <p>■詳細</p>{goal.detail}
+                    <div className={commonStyles.mgt20}/>
+                    <p>■期限</p>{goal.deadline}
+                    <div className={commonStyles.mgt20}/>
+                    {/*本人のみ目標編集画面への遷移を表示*/}
+                    {
+                      loginUserId === user.id &&
+                      <Link href={`/updategoal/${goal.id}`}>
+                        <a>目標を更新する</a>
+                      </Link>
+                    }
+                  </Card>
+                )
+              })
+            }
+          </>
+        </Card>
+      </div>
+    </Layout>
+  )
+}
+
+export default UserId
