aa5de24b7e657e9079f3787b1c04f40731fad1e2
KeisukeKashima <k2041kei@gmail.com>
2022-05-06 14:49:08 +0900

    add ローカルdocker-composeで動かすまで

--- util/axiosClient.ts
+++ util/axiosClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios"
 
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  // next.configでAPP_ENVを設定している
+  baseURL: process.env.serviceOrigin,
   // リクエストに Cookie を添えて送信するために withCredentials オプションを有効にする(これがないとapi側でセットしたクッキーがブラウザに保存されない)
   withCredentials: true,
   responseType: "json",
